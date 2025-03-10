e58df37cdabc39d3c7e72c99aedf0ae3e13b76c3
yaredow <yaredyilma11@gmail.com>
2025-03-10 21:54:17 +0300

    finish configuring sidebar for dashboard and profile

---
+++ src/modules/dashboard/ui/components/app-sidebar.tsx
@@ -0,0 +1,38 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { routes } from "../../constants";
+import Link from "next/link";
+
+export const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {routes.map((route) => (
+                <SidebarMenuItem key={route.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={route.href}>
+                      <route.icon />
+                      <span>{route.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
--- src/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ src/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -1,9 +1,20 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "../components/app-sidebar";
+
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
 };
 
 export default DashboardLayout;
